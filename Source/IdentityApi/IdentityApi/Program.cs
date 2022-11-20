249f565cdbda97351d0697357012b2ea7fd066ed
Emil Andersen <emil.rauff.andersen@hotmail.com>
2022-11-20 12:23:25 +0100

    Revert "Added serilog"
    This reverts commit 09c3a4bed07e6f58d2079a25a5f13edc6b2538c9.

--- Source/IdentityApi/IdentityApi/Program.cs
+++ Source/IdentityApi/IdentityApi/Program.cs
@@ -1,8 +1,3 @@
-using Serilog;
-using Serilog.Sinks.Elasticsearch;
-using System.Reflection;
-using System;
-
 namespace IdentityApi
 {
     public class Program
@@ -11,35 +6,12 @@ namespace IdentityApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile(
-                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-                    optional: true)
-                .Build();
-
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            // Add serilog for logging
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .Enrich.WithMachineName()
-                .Enrich.WithEnvironmentName()
-                .WriteTo.Console()
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-                {
-                    AutoRegisterTemplate = true,
-                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".", "-")}-{builder.Environment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-                })
-                .ReadFrom.Configuration(configuration)
-                .CreateBootstrapLogger();
-
-            builder.Host.UseSerilog();
 
             var app = builder.Build();
 
