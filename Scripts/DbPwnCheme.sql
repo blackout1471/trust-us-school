c5deab4dbf7041d97000b8471c49c2efe14a71fb
Emil Andersen <emil.rauff.andersen@hotmail.com>
2022-11-23 11:06:18 +0100

    Added erd

---
+++ Scripts/DbPwnCheme.sql
@@ -0,0 +1,44 @@
+USE master
+GO
+
+-- Check if trust us database exists
+-- if it doesnt exists create it
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'StoredPasswords')
+BEGIN
+  CREATE DATABASE StoredPasswords;
+END;
+GO
+
+-- use the newly created database
+use StoredPasswords;
+go
+
+-- Drop pre exisitng tables if they exist
+DROP TABLE IF EXISTS [dbo].LeakedPasswords;
+
+/*
+*
+* Create tables
+*
+*/
+create table LeakedPasswords(
+  HashedPassword varchar(60) PRIMARY KEY,
+  PwnCount integer
+)
+
+
+/*
+* Create stored procedure to get hashedpassword from hashed password
+*/
+DROP PROCEDURE IF EXISTS dbo.SP_HashedPasswordExists
+
+go
+
+CREATE PROCEDURE SP_HashedPasswordExists 
+@HashedPassword varchar(80)
+AS
+
+Select COUNT(*) from dbo.LeakedPasswords
+WITH (NOLOCK)
+where HashedPassword = @HashedPassword
+GO
