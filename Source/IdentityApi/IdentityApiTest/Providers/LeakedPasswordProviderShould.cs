e999071b3c653e4dfa4d6f3ffa9440865cff25fb
Emil Andersen <emil.rauff.andersen@hotmail.com>
2022-11-23 14:34:41 +0100

    Added unit test for provider

--- Source/IdentityApi/IdentityApiTest/Providers/LeakedPasswordProviderShould.cs
+++ Source/IdentityApi/IdentityApiTest/Providers/LeakedPasswordProviderShould.cs
@@ -1,12 +1,87 @@
-﻿namespace IdentityApiUnitTest.Providers
+﻿using FakeItEasy;
+using IdentityApi.DbModels;
+using IdentityApi.Providers;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace IdentityApiUnitTest.Providers
 {
     public class LeakedPasswordProviderShould
     {
-        public LeakedPasswordProviderShould()
-        {
+        public LeakedPasswordProviderShould() {}
 
+        [Fact]
+        public async Task ExpectFalse_WhenDatatableRowIs0_GetIsPasswordLeakedAsync()
+        {
+            // Arrange
+            var dataResult = new DataTable();
+            var provider = new FakeSqlProvider();
+            provider.Result = dataResult;
+
+            // Act
+            var expected = await provider.GetIsPasswordLeakedAsync("");
+
+            // Assert
+            Assert.False(expected);
         }
 
-        
+        [Fact]
+        public async Task ExpectTrue_WhenDatatableResultIs1_GetIsPasswordLeakedAsync()
+        {
+            // Arrange
+            var dataResult = new DataTable();
+            dataResult.Columns.Add(new DataColumn("Col1", typeof(int)));
+            
+            var row = dataResult.NewRow();
+            row[0] = 1;
+            dataResult.Rows.Add(row);
+
+            var provider = new FakeSqlProvider();
+            provider.Result = dataResult;
+
+            // Act
+            var expected = await provider.GetIsPasswordLeakedAsync("");
+
+            // Assert
+            Assert.True(expected);
+        }
+
+        [Fact]
+        public async Task ExpectFalse_WhenDatatableResultIs0_GetIsPasswordLeakedAsync()
+        {
+            // Arrange
+            var dataResult = new DataTable();
+            dataResult.Columns.Add(new DataColumn("Col1", typeof(int)));
+
+            var row = dataResult.NewRow();
+            row[0] = 0;
+            dataResult.Rows.Add(row);
+
+            var provider = new FakeSqlProvider();
+            provider.Result = dataResult;
+
+            // Act
+            var expected = await provider.GetIsPasswordLeakedAsync("");
+
+            // Assert
+            Assert.False(expected);
+        }
+    }
+
+    internal class FakeSqlProvider : LeakedPasswordProvider
+    {
+        public DataTable Result { get; set; } = default!;
+
+        public FakeSqlProvider() : base(null) {}
+
+        protected override Task<DataTable> RunQueryAsync(string query)
+        {
+            return Task.FromResult(Result);
+        }
+
+        protected override Task<DataTable> RunSpAsync(string spName, params SpElement[] spElements)
+        {
+            return Task.FromResult(Result);
+        }
     }
 }
